a9ee4b6029cb1a58a5f32d7487045ae85f0b7085
dengshunlai <mu3305495@163.com>
2016-06-30 22:40:50 +0800

    Initial Commit

---
+++ DSLArchiver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSLArchiver
+//
+//  Created by 邓顺来 on 16/6/30.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
